f79b451bf6708fa8b94d0338154750e564d172d6
loyal <1023730181@qq.com>
2018-05-04 16:09:26 +0800

    Add app for lists, with deliberately failing with unit test.

--- functional_tests.py
+++ functional_tests.py
@@ -4,6 +4,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
