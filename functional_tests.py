ec6811223ce6b6e1c6c57d80f873145d06131675
loyal <1023730181@qq.com>
2018-05-04 15:59:13 +0800

    add a new unitest case

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_on_start_a_list_and_retrive_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('TO-DO', self.browser.title)
+
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
